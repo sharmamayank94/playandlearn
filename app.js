d80349edda357fec1ef9e4b27e3aff975d59d5a8
MAYANK <sharmamayank941@gmail.com>
2020-11-18 23:32:32 +0530

    server created and grid game started

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var ejs = require('ejs');
+
+var app = express();
+
+app.set('view engine', 'ejs');
+app.use('/public', express.static('static'));
+
+app.get('/', (req, res)=>{
+	res.render("home", {tab: "home"});
+});
+
+app.get("/calculaterace", (req, res)=>{
+	res.render("calculaterace", {tab: "race"});
+});
+
+app.get("/gridandfun", (req, res)=>{
+	res.render("gridandfun", {tab: "gridandfun"});
+})
+
+app.listen(3000, (a,b)=>{
+	console.log("Server is running on port 3000");
+});
